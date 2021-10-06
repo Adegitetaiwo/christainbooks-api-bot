4cd72fa627450360f63b42ac2f360ba4c6e02270
Adegite Taiwo <adegitetaiwo24@gmail.com>
2021-10-07 00:27:28 +0100

    first deployment

--- mychristianbooks_bot/settings.py
+++ mychristianbooks_bot/settings.py
@@ -16,6 +16,7 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,3 +157,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
