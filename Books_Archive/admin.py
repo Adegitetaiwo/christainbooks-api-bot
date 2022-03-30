68d81fe6e86984f7f492400209cd247d43a11335
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-03-30 15:54:22 +0100

    make it better

--- Books_Archive/admin.py
+++ Books_Archive/admin.py
@@ -3,4 +3,8 @@ from .models import Books, Author
 
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Books)
+
+@admin.register(Books)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ["title", "author"]
+    list_filter = ["author"]
--- mychristianbooks_bot/settings.py
+++ mychristianbooks_bot/settings.py
@@ -108,8 +108,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'telegram-bot-api',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'ADE15112000gite +',
+        'PORT': '3305'
     }
 }
 
