c42e921cfc6489117775083f249c72f7bb02542c
Adegite Taiwo <adegitetaiwo24@gmail.com>
2022-05-06 05:15:24 +0100

    added Sentry support

--- mychristianbooks_bot/settings.py
+++ mychristianbooks_bot/settings.py
@@ -18,6 +18,22 @@ import cloudinary.uploader
 import cloudinary.api
 from dotenv import load_dotenv
 import django_heroku
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://5662459e68294bccaefb16aeebc65863@o420940.ingest.sentry.io/6383613",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 load_dotenv()  # take environment variables from .env.
 
